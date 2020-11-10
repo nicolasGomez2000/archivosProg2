0ff0cae00ecbc5fc7667ccd26db5c5dffe142891
Nicolas Gomez <nico.2000.03.nmg10@gmail.com>
2020-11-10 18:16:42 -0300

    completando practicas 1-6

--- practica6.py
+++ practica6.py
@@ -1,3 +1,4 @@
+import random as rnd
 def apariciones(l,e):
     if len(l) == 0:
         return 0
@@ -82,13 +83,131 @@ def contar(s):
     for i in l:
         if i not in ya_contados:
             d[i] = apariciones(l,i)
-            print(apariciones(l,i))
             ya_contados.append(i)
-    print(d)
     return d
 def test_contar():
     assert contar("que que que") == { "que": 3}
     assert contar("que lindo dia que hace hoy") == { "que": 2, "lindo": 1, "dia": 1, "hace": 1, "hoy": 1}
+def eliminar_caracter(l,s):
+    if len(l) == 0:
+        return ""
+    if l[0] == s:
+        return eliminar_caracter(l[1:],s)
+    else:
+        return l[0] + eliminar_caracter(l[1:],s) 
 
-
+def contar_c(s):
+    d = dict()
+    s = eliminar_caracter(s," ")
+    print(s)
+    ya_contados = []
+    for i in s:
+        if i not in ya_contados:
+            d[i] = apariciones(s,i)
+            ya_contados.append(i)
+    return d 
+def test_cc():
+    assert contar_c("hola como te va") == {"h":1,"o":3,"l":1,"a":2,"c":1,"m":1,"t":1,"e":1,"v":1}
+def caracteres(s):
+    c = []
+    for i in s:
+        if i not in c:
+            c.append(i)
+    return c
     
+def caracter_palabra(s):
+    l = s.split(" ")
+    lc = caracteres(eliminar_caracter(s," "))
+    d = dict()
+    for i in lc:
+        d[i] = ""
+        for j in l:
+            if i in j:
+                if len(d[i]) < len(j):
+                    d[i] = j
+    return d
+
+def test_cp():
+    assert caracter_palabra("hola como estas") == {"h":"hola","o":"hola","l":"hola","a":"estas",
+                                                    "c":"como","m":"como","e":"estas","s":"estas",
+                                                    "t":"estas"} 
+
+def head(name,n):
+    a = open("txt_practica/" + name,"r")
+    print(a.read(n))
+
+def cp(a1,a2):
+    f1 = open("txt_practica/" + a1 ,"r")
+    s = f1.read()
+    f1.close()
+    f2 = open("txt_practica/" + a2,"w")
+    f2.write(s)
+    f2.close()
+
+def wc(name):
+    ar = open("txt_practica/" + name,"r")
+    lineas = len(ar.readlines())   
+    ar.seek(0)
+    palabras = 0
+    for i in ar:
+        palabras += len(i.split(" "))
+    ar.seek(0)
+    caracteres = len(eliminar_caracter(ar.read()," "))
+
+    print("El archvo " + name + " tiene " + str(lineas) + " lineas, " 
+           + str(palabras) + " palabras y  " + str(caracteres) + " caracteres." )
+    ar.close()
+
+def greep(name,s):
+    ar = open("txt_practica/" + name,"r")
+    for i in ar.readlines():
+        if s in i.split(" "):
+            print(i)
+    ar.close()
+
+def cifrar(s):
+    texto_cifrado = ""
+    for i in s:
+        if i.isalpha():
+            texto_cifrado += chr(((ord(i) + 13) % 26)+32)
+        else: texto_cifrado += i
+    return texto_cifrado
+print(cifrar("queee onda"))
+    
+def rot13(name_origen,name_destino):
+    origen = open("txt_practica/" + name_origen,"r")
+    texto_cifrado = []
+    for i in origen.readlines():
+        texto_cifrado.append(cifrar(i))
+    origen.close()
+    destino = open("txt_practica/" + name_destino,"w")
+    for i in texto_cifrado:
+        destino.write(i)
+    destino.close()
+    
+def cargarDatos(name):
+    ar = open("txt_practica/" + name,"r")
+    usuarios = dict()
+    for i in ar.readlines():
+        datos = i.split(" ")
+        datos[1] = eliminar_caracter(datos[1],"\n")
+        usuarios[datos[0]] = datos[1]
+    ar.close()
+    return usuarios
+def test_cd():
+    assert cargarDatos("usuarios.txt") == {"nicolas":"123","pablo":"321",
+                                          "pepe":"pepe123","marcelo":"7676lalalele001",
+                                          "tomas":"39241578","maria":"airam"}
+def agregar_usuarios(d, name):
+    ar1 = open("txt_practica/" + name, "a")
+    for u,c in d.items():
+        ar1.write(u + " " + c + "\n")
+    ar1.close()
+
+agregar_usuarios({"jeremias":"4353","steven":"oeoda3",
+                                          "john":"454fds","emma":"emma324121",
+                                          "vanesa":"4341","alex":"99312"},"usuarios.txt")
+
+
+        
+
